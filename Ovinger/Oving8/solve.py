06060c304bfe1402a12c013714e35641304d8b7f
simenbkr <simen@haitech.org>
2016-10-19 19:08:24 +0200

    Litta fiks.

--- Ovinger/Oving8/solve.py
+++ Ovinger/Oving8/solve.py
@@ -1,26 +1,29 @@
 #!/usr/bin/python3
 
 Inf = 1000000000
+memo2 = {}
 def min_coins_greedy(coins, value):
-	coins = sorted(coins)[::-1]
-	number = 0
-	myvalue = 0
-	for i in range(len(coins)):
-		count = 0
-		while (count+1)*coins[i] <= value and myvalue+(count+1)*coins[i] <= value:
-			count +=1
-		myvalue += count*coins[i]
-		number += count	
-	return number
+	if value in memo2: return memo2[value]
+	antall, indeks = 0, 0
+	while value > 0:
+		if value/coins[indeks] >= 1:
+			antall += value//coins[indeks]
+			value %= coins[indeks]
+		indeks += 1
+	memo2[value] = antall
+	return antall
 
+memo = {}
 def min_coins_dynamic(coins, value, length):
-		
+	if value in memo: return memo[value]
 	antall = [0]
 	for i in range(1,value+1):
 		antall.append(antall[i-1] + 1)
 		for mynt in coins:
 			if mynt <= i and antall[i-mynt] + 1 < antall[i]:
 				antall[i] = antall[i-mynt] + 1	
+				memo[i] = antall[i]
+	memo[value] = antall[value]
 	return antall[value]
 
 def can_use_greedy(coins):
@@ -34,8 +37,7 @@ def main():
 	coins = []
 	for c in stdin.readline().split():
 		coins.append(int(c))
-	coins.sort()
-	coins.reverse()
+	coins.sort(reverse=True)
 	method = stdin.readline().strip()
 	if method == "graadig" or (method == "velg" and can_use_greedy(coins)):
 		for line in stdin:
@@ -44,6 +46,5 @@ def main():
 		for line in stdin:
 			print(min_coins_dynamic(coins, int(line), len(coins)))
 			
-			
-if __name__ == '__main__':
-	main()
+
+main()
